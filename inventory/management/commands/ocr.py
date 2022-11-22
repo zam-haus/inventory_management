d5c6bfebcd2f166710cf90810604291acc809812
Julian <cod3monk@users.noreply.github.com>
2022-11-22 15:07:00 +0100

    First draft of OCR command (#18)
    Co-authored-by: Daniel Oeh <968613+danieloeh@users.noreply.github.com>

---
+++ inventory/management/commands/ocr.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand, CommandError
+from inventory.models import ItemImage
+from tqdm import tqdm
+import multiprocessing as mp
+from multiprocessing import Pool
+import pytesseract
+import logging
+
+def run_ocr(id_with_path):
+    image_id, path = id_with_path
+    return (image_id, pytesseract.image_to_string(path))
+
+class Command(BaseCommand):
+    help = "Runs OCR on item images"
+
+    def get_logger(self):
+        logger = logging.getLogger("ocr")
+        logger.handlers.clear()
+        handler = logging.StreamHandler(self.stdout)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.INFO)
+        return logger
+        
+
+    def handle(self, *args, **kwargs):
+        logger = self.get_logger()
+        mp.set_start_method("fork")
+
+        logger.info("Collecting image paths...")
+        id_to_path = {image.id : image.image.path for image in ItemImage.objects.all()}
+        logging.info("Done")
+        with Pool(4) as pool:
+            id_to_ocr_text = dict(list(tqdm(pool.imap(func=run_ocr, iterable=id_to_path.items()), total=len(id_to_path))))
+        logging.info("Writing results to database")
+        for image_id, ocr_text in tqdm(id_to_ocr_text.items()):
+            image_db = ItemImage.objects.get(id=image_id)
+            image_db.ocr_text = ocr_text
+            image_db.save()
+        
